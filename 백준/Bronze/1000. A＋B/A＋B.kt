39dfe227a750695e818a8a0934268845a4a33e0b
kim-jerok <kim.jerok@gmail.com>
2025-06-24 02:07:11 +0900

    [Bronze V] Title: A+B, Time: 116 ms, Memory: 21608 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.kt
@@ -0,0 +1,11 @@
+fun inputIntPair(): Pair<Int, Int> {
+    val inputIntList = readln()
+        .split(" ")
+        .map { it.toInt() }
+    return Pair(inputIntList[0], inputIntList[1])
+}
+
+fun main() {
+    val (a, b) = inputIntPair()
+    println(a + b)
+}
