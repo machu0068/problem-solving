c1e348bff27a08096a2cd9f5c9528448314da8cd
kim-jerok <kim.jerok@gmail.com>
2025-06-30 21:08:48 +0900

    [Bronze II] Title: 공 바꾸기, Time: 144 ms, Memory: 24024 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.kt
@@ -0,0 +1,23 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+    val (n, m) = reader.readLine().split(" ").map { it.toInt() }
+    val array = Array(n) { it + 1 }
+
+    repeat(m) {
+        val (i, j) = reader.readLine().split(" ").map { it.toInt() }
+        array[i - 1] = array[j - 1].also { array[j - 1] = array[i - 1] }
+    }
+
+    writer.write(array.joinToString(" "))
+
+    writer.flush()
+    writer.close()
+    reader.close()
+}
