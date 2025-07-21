8128f8cb6d684c7ff5fdc5a8667583861f846b6b
machu0068 <kim.jerok@gmail.com>
2025-07-21 23:02:16 +0900

    Title: 친근하지 않은 수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250721/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+            count++;
+        }
+    }
+    cout << count;
+    return 0;
+}
