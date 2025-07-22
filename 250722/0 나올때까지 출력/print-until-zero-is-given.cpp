57c60bda90663280a67c7dd3a301bde8c6e48db1
machu0068 <kim.jerok@gmail.com>
2025-07-22 11:21:01 +0900

    Title: 0 나올때까지 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250722/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = -1;
+    while (n != 0) {
+        cin >> n;
+        if (n == 0) {
+            break;
+        }
+        cout << n << endl;
+    }
+    return 0;
+}
