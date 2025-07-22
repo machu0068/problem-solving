997b235d4686f33e4a0b2afb4f0b171d1518a0ad
machu0068 <kim.jerok@gmail.com>
2025-07-22 16:08:50 +0900

    Title: 숫자 피라미드 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250722/숫자 피라미드 2/number-pyramid-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int num = 0;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < i; j++) {
+            num++;
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
