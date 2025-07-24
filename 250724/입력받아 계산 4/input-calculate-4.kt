1598589a8ac663dd89a090fce24f639ff2cd68ef
machu0068 <kim.jerok@gmail.com>
2025-07-24 14:54:56 +0900

    Title: 입력받아 계산 4, Time: 108ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250724/입력받아 계산 4/input-calculate-4.kt
@@ -0,0 +1,14 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+
+fun main() {
+    BufferedReader(System.`in`.reader()).use { br ->
+        BufferedWriter(System.out.writer()).use { bw ->
+            val a = br.readLine().toInt()
+            val output = buildString {
+                append(a * 2)
+            }
+            bw.write(output)
+        }
+    }
+}
