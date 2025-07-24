bd245d188dd50a3e5f236b09d16c56d57c5fef83
machu0068 <kim.jerok@gmail.com>
2025-07-24 14:09:26 +0900

    Title: 길이 단위 변환하기, Time: 110ms, Memory: 10MB, Status: Passed - Codetree

--- 250724/길이 단위 변환하기/change-length-unit.kt
+++ 250724/길이 단위 변환하기/change-length-unit.kt
@@ -2,11 +2,13 @@ import java.io.BufferedWriter
 
 fun main() {
     BufferedWriter(System.out.writer()).use {
+        val a = 9.2
+        val b = 1.3
         val ft = 30.48
         val mi = 160934
         val output = buildString {
-            appendLine("9.2ft = %.1fcm".format(9.2 * ft))
-            appendLine("1.3mi = %.1fcm".format(1.3 * mi))
+            appendLine("%.1fft = %.1fcm".format(a, a * ft))
+            appendLine("%.1fmi = %.1fcm".format(b, b * mi))
         }
         it.write(output)
     }
