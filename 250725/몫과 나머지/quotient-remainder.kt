e16e38e65f191793ccca56573c975e703649cda9
machu0068 <kim.jerok@gmail.com>
2025-07-25 13:38:36 +0900

    Title: 몫과 나머지, Time: 97ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250725/몫과 나머지/quotient-remainder.kt
@@ -0,0 +1,17 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.util.StringTokenizer
+
+fun main() {
+    BufferedReader(System.`in`.reader()).use { br ->
+        BufferedWriter(System.out.writer()).use { bw ->
+            val st = StringTokenizer(br.readLine())
+            val a = st.nextToken().toInt()
+            val b = st.nextToken().toInt()
+            val output = buildString {
+                append("${a / b}...${a % b}")
+            }
+            bw.write(output)
+        }
+    }
+}
