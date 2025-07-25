0e992ebfb410c5dad7d40b916d7217b06fc0055e
machu0068 <kim.jerok@gmail.com>
2025-07-25 10:16:06 +0900

    Title: 문자 받아 출력, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250725/문자 받아 출력/enter-char-and-print.kt
@@ -0,0 +1,11 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+
+fun main() {
+    BufferedReader(System.`in`.reader()).use { br ->
+        BufferedWriter(System.out.writer()).use { bw ->
+            val c = br.read()
+            bw.write(c)
+        }
+    }
+}
