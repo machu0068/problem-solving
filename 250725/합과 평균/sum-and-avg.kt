12c399c38e16b2b842edca78d726d2bb674444e9
machu0068 <kim.jerok@gmail.com>
2025-07-25 13:51:51 +0900

    Title: 합과 평균, Time: 106ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250725/합과 평균/sum-and-avg.kt
@@ -0,0 +1,14 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.util.StringTokenizer
+
+fun main() {
+    BufferedReader(System.`in`.reader()).use { br ->
+        BufferedWriter(System.out.writer()).use { bw ->
+            val st = StringTokenizer(br.readLine())
+            val a = st.nextToken().toInt()
+            val b = st.nextToken().toInt()
+            bw.write("%d %.1f".format(a + b, (a + b) / 2.0))
+        }
+    }
+}
