ba8a408f138cc1a686fa52fc6f1d08b8c2c2c594
machu0068 <kim.jerok@gmail.com>
2025-07-26 01:16:27 +0900

    Title: 특정 숫자 도달하기 2, Time: 148ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250726/특정 숫자 도달하기 2/reaching-specific-number-2.kt
@@ -0,0 +1,10 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+
+fun main() {
+    val n = BufferedReader(System.`in`.reader()).use {
+        it.readLine().toInt()
+    }
+    val output = (n downTo 1).joinToString(" ")
+    BufferedWriter(System.out.writer()).use { it.write(output) }
+}
