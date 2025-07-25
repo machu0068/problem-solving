c0a4c457f8c4157ed65f5588ce04b7e05ce6140a
machu0068 <kim.jerok@gmail.com>
2025-07-25 13:30:47 +0900

    Title: 간단한 사칙연산, Time: 97ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250725/간단한 사칙연산/simple-arithmetic-operation.kt
@@ -0,0 +1,22 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.util.StringTokenizer
+
+fun main() {
+    BufferedReader(System.`in`.reader()).use { br ->
+        BufferedWriter(System.out.writer()).use { bw ->
+            val st = StringTokenizer(br.readLine())
+            val (a, b) = Pair(
+                st.nextToken().toInt(),
+                st.nextToken().toInt()
+            )
+            val output = buildString {
+                appendLine(a + b)
+                appendLine(a - b)
+                appendLine(a / b)
+                appendLine(a % b)
+            }
+            bw.write(output)
+        }
+    }
+}
