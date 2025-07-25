3e3adc34bf4cc805f44fb291b3f9c06a9a506c68
machu0068 <kim.jerok@gmail.com>
2025-07-25 15:50:36 +0900

    Title: 4가지 관계연산자, Time: 157ms, Memory: 18MB, Status: Passed - Codetree

---
+++ 250725/4가지 관계연산자/four-relational-operators.kt
@@ -0,0 +1,17 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.util.StringTokenizer
+
+fun main() {
+    val (a, b) = BufferedReader(System.`in`.reader()).use {
+        val st = StringTokenizer(it.readLine())
+        Pair(st.nextToken().toInt(), st.nextToken().toInt())
+    }
+    val output = listOf(
+        if (a >= b) 1 else 0,
+        if (a > b) 1 else 0,
+        if (a <= b) 1 else 0,
+        if (a < b) 1 else 0
+    ).joinToString("\n")
+    BufferedWriter(System.out.writer()).use { it.write(output) }
+}
