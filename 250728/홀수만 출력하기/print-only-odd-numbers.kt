459ea07865f62ea7a39158040e47c873b478ec6e
machu0068 <kim.jerok@gmail.com>
2025-07-28 11:17:41 +0900

    Title: 홀수만 출력하기, Time: 130ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 250728/홀수만 출력하기/print-only-odd-numbers.kt
@@ -0,0 +1,15 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+
+fun main() {
+    BufferedReader(System.`in`.reader()).use { br ->
+        BufferedWriter(System.out.writer()).use { bw ->
+            val n = br.readLine().toInt()
+            val array = Array(n) { br.readLine().toInt() }
+            val output = array
+                .filter { it % 2 == 1 && it % 3 == 0 }
+                .joinToString("\n")
+            bw.write(output)
+        }
+    }
+}
