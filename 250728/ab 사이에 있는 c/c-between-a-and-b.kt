5e7f369a9b36fdc4ee79546aab38f7f13dc58f9c
machu0068 <kim.jerok@gmail.com>
2025-07-28 21:29:48 +0900

    Title: ab 사이에 있는 c, Time: 99ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250728/ab 사이에 있는 c/c-between-a-and-b.kt
@@ -0,0 +1,16 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.util.StringTokenizer
+
+fun main() {
+    BufferedReader(System.`in`.reader()).use { br ->
+        val st = StringTokenizer(br.readLine())
+        val range = (st.nextToken().toInt()..st.nextToken().toInt())
+        val c = st.nextToken().toInt()
+        val output = when (range.firstOrNull { it % c == 0 } != null) {
+            true -> "YES"
+            false -> "NO"
+        }
+        BufferedWriter(System.out.writer()).use { it.write(output) }
+    }
+}
