692cd5e9ca2da1528a3ce7e6aec2c3d22126db13
machu0068 <kim.jerok@gmail.com>
2025-07-29 15:06:05 +0900

    Title: 좌우반전시킨 정사각형, Time: 148ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250729/좌우반전시킨 정사각형/left-right-inverted-square.kt
@@ -0,0 +1,12 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+
+fun main() {
+    val n = BufferedReader(System.`in`.reader()).use { it.readLine().toInt() }
+    val output = Array(n) { row ->
+        Array(n) { col ->
+            (n - col) * (row + 1)
+        }.joinToString(" ")
+    }.joinToString("\n")
+    BufferedWriter(System.out.writer()).use { it.write(output) }
+}
