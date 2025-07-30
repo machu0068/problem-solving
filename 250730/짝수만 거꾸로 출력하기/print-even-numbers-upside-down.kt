0a18b1e79b12a5d5821aef8d4c073a629681fb78
machu0068 <kim.jerok@gmail.com>
2025-07-30 22:08:28 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 130ms, Memory: 12MB, Status: Passed - Codetree

---
+++ 250730/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.kt
@@ -0,0 +1,17 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.util.StringTokenizer
+
+fun main() {
+    BufferedReader(System.`in`.reader()).use { br ->
+        BufferedWriter(System.out.writer()).use { bw ->
+            val n = br.readLine().toInt()
+            val st = StringTokenizer(br.readLine())
+            val output = Array(n) { st.nextToken().toInt() }
+                .filter { it % 2 == 0 }
+                .reversed()
+                .joinToString(" ")
+            bw.write(output)
+        }
+    }
+}
