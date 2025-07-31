0fdaa305f18ecb15914f9efcd1526230114d84ad
machu0068 <kim.jerok@gmail.com>
2025-07-31 13:56:23 +0900

    Title: 십의 자리 숫자, Time: 167ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250731/십의 자리 숫자/number-of-tens-digit.kt
@@ -0,0 +1,16 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.util.StringTokenizer
+
+fun main() {
+    BufferedReader(System.`in`.reader()).use { br ->
+        val st = StringTokenizer(br.readLine())
+        val map = generateSequence { st.nextToken().toInt() }
+            .takeWhile { it != 0 }
+            .map { (it / 10) % 10 }
+            .groupingBy { it }
+            .eachCount()
+        val output = (1..9).joinToString("\n") { "$it - ${map[it] ?: 0}" }
+        BufferedWriter(System.out.writer()).use { it.write(output) }
+    }
+}
