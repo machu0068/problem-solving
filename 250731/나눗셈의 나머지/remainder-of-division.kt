1b2bc917df50dff31921d625df4cff18478ed21f
machu0068 <kim.jerok@gmail.com>
2025-07-31 14:58:02 +0900

    Title: 나눗셈의 나머지, Time: 167ms, Memory: 14MB, Status: Wrong Answer - Codetree

---
+++ 250731/나눗셈의 나머지/remainder-of-division.kt
@@ -0,0 +1,17 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.util.StringTokenizer
+
+fun main() {
+    val (a, b) = BufferedReader(System.`in`.reader()).use { br ->
+        val st = StringTokenizer(br.readLine())
+        st.nextToken().toInt() to st.nextToken().toInt()
+    }
+    val map = generateSequence(a / b to a % b) { it.first / b to it.first % b }
+        .takeWhile { !(it.first == 0 && it.second == 0) }
+        .map { it.second }
+        .groupingBy { it }
+        .eachCount()
+    val output = map.values.toList().sumOf { it * it }.toString()
+    BufferedWriter(System.out.writer()).use { it.write(output) }
+}
